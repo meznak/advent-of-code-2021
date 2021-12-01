aca735d624e593c30bd412de911ed673b8991b2d
Nathan Plamondon <nplamondon@gmail.com>
2021-11-30 23:09:34 -0700

    day01: Solve part 2

--- day01/main.go
+++ day01/main.go
@@ -31,10 +31,22 @@ func SolvePart1(input []int) int {
 	return increases
 }
 
-// ???
+// Count rolling sum increases
 func SolvePart2(input []int) int {
+	sums := SumWindows(input)
+	return SolvePart1(sums)
+}
 
-	return -1
+// Sum a three-measurement rolling window
+func SumWindows(input []int) (sums []int) {
+	for i := range input {
+		// stop when the last value has been read
+		if i < len(input)-2 {
+			sums = append(sums, input[i]+input[i+1]+input[i+2])
+		}
+	}
+
+	return
 }
 
 func main() {
