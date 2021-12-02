173ded2da04024ace0c7e23f3a0d03702e7d0f5b
Nathan Plamondon <nplamondon@gmail.com>
2021-12-01 22:35:56 -0700

    Add benchmarking tests

--- day00/main_test.go
+++ day00/main_test.go
@@ -24,3 +24,21 @@ func Test_SolvePart2(t *testing.T) {
 		t.Errorf("ReadInput() = %v, want %v", got, want)
 	}
 }
+
+func Benchmark_SolvePart1(b *testing.B) {
+	input := shared.ReadInputInts("input")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = SolvePart1(input)
+	}
+}
+
+func Benchmark_SolvePart2(b *testing.B) {
+	input := shared.ReadInputInts("input")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = SolvePart2(input)
+	}
+}
