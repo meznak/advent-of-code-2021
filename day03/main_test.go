a098ac471dca5cc97861f9f47cea166a20fcfa31
Nathan Plamondon <nplamondon@gmail.com>
2021-12-02 22:51:43 -0700

    day03: Solve part 1

---
+++ day03/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"aoc2021/shared"
+	"reflect"
+	"testing"
+)
+
+func Test_SolvePart1(t *testing.T) {
+	want := 198
+
+	input := shared.ReadInputLines("sample1")
+
+	if got := SolvePart1(input); !reflect.DeepEqual(got, want) {
+		t.Errorf("ReadInput() = %v, want %v", got, want)
+	}
+}
+func Test_SolvePart2(t *testing.T) {
+	want := -1
+
+	input := shared.ReadInputLines("sample1")
+
+	if got := SolvePart2(input); !reflect.DeepEqual(got, want) {
+		t.Errorf("ReadInput() = %v, want %v", got, want)
+	}
+}
+
+func Benchmark_SolvePart1(b *testing.B) {
+	input := shared.ReadInputLines("sample1")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = SolvePart1(input)
+	}
+}
+
+func Benchmark_SolvePart2(b *testing.B) {
+	input := shared.ReadInputLines("sample1")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = SolvePart2(input)
+	}
+}
