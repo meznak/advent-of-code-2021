6da1530e3d9ec2b0a634315eee06b37aee3a61e8
Nathan Plamondon <nplamondon@gmail.com>
2021-12-03 01:17:21 -0700

    day03: Solve part 2

--- day03/main_test.go
+++ day03/main_test.go
@@ -12,21 +12,47 @@ func Test_SolvePart1(t *testing.T) {
 	input := shared.ReadInputLines("sample1")
 
 	if got := SolvePart1(input); !reflect.DeepEqual(got, want) {
-		t.Errorf("ReadInput() = %v, want %v", got, want)
+		t.Errorf("SolvePart1() = %T(%v), want %T(%v)", got, got, want, want)
 	}
 }
 func Test_SolvePart2(t *testing.T) {
-	want := -1
+	want := 230
 
 	input := shared.ReadInputLines("sample1")
 
 	if got := SolvePart2(input); !reflect.DeepEqual(got, want) {
-		t.Errorf("ReadInput() = %v, want %v", got, want)
+		t.Errorf("SolvePart2() = %T(%v), want %T(%v)", got, got, want, want)
+	}
+}
+
+func Test_MostCommonBit(t *testing.T) {
+	want := "10110"
+
+	input := shared.ReadInputLines("sample1")
+
+	var got string
+
+	for i := range input[0] {
+		got += string(MostCommonBit(input, i))
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("MostCommonBit() = %T(%v), want %T(%v)", got, got, want, want)
+	}
+}
+
+func Test_MostCommonBits(t *testing.T) {
+	want := "10110"
+
+	input := shared.ReadInputLines("sample1")
+
+	if got := MostCommonBits(input); !reflect.DeepEqual(got, want) {
+		t.Errorf("MostCommonBits() = %T(%v), want %T(%v)", got, got, want, want)
 	}
 }
 
 func Benchmark_SolvePart1(b *testing.B) {
-	input := shared.ReadInputLines("sample1")
+	input := shared.ReadInputLines("input")
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -35,7 +61,7 @@ func Benchmark_SolvePart1(b *testing.B) {
 }
 
 func Benchmark_SolvePart2(b *testing.B) {
-	input := shared.ReadInputLines("sample1")
+	input := shared.ReadInputLines("input")
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
