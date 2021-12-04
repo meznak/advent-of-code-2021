0c01c51b653c0e732f4445dfeca300df1548dded
Nathan Plamondon <nplamondon@gmail.com>
2021-12-03 23:48:08 -0700

    day04: Mark called numbers on boards

--- day04/main.go
+++ day04/main.go
@@ -8,10 +8,25 @@ package main
 import (
 	"aoc2021/shared"
 	"fmt"
+	"strings"
 )
 
 // Convert binary to "gamma" and "epsilon" numbers
 func SolvePart1(input []string) int {
+
+	numbers := ParseNumbers(input[0])
+	boards := ParseBoards(input[2:])
+
+	board_state := make([][5][5]bool, len(boards))
+	for i := range board_state {
+		for j := 0; j < 5; j++ {
+			board_state[i][j] = [5]bool{false, false, false, false, false}
+		}
+	}
+
+	for _, number := range numbers {
+		CheckBoards(number, &boards, &board_state)
+	}
 
 	return -1
 }
@@ -19,6 +34,41 @@ func SolvePart1(input []string) int {
 //
 func SolvePart2(input []string) int {
 	return -1
+}
+
+func ParseNumbers(input string) (output []string) {
+	return strings.Split(input, ",")
+}
+
+func ParseBoards(input []string) (output [][][]string) {
+	board := make([][]string, 0)
+
+	for _, line := range input {
+		if line == "" {
+			continue
+		}
+
+		board = append(board, strings.Fields(line))
+
+		if len(board) == 5 {
+			output = append(output, board)
+			board = make([][]string, 0)
+		}
+	}
+
+	return
+}
+
+func CheckBoards(number string, boards *[][][]string, board_state *[][5][5]bool) {
+	for i, board := range *boards {
+		for j := range board {
+			for k := range board[0] {
+				if board[j][k] == number {
+					(*board_state)[i][j][k] = true
+				}
+			}
+		}
+	}
 }
 
 func main() {
--- day04/main_test.go
+++ day04/main_test.go
@@ -15,6 +15,7 @@ func Test_SolvePart1(t *testing.T) {
 		t.Errorf("SolvePart1() = %T(%v), want %T(%v)", got, got, want, want)
 	}
 }
+
 func Test_SolvePart2(t *testing.T) {
 	want := -1
 
@@ -22,6 +23,81 @@ func Test_SolvePart2(t *testing.T) {
 
 	if got := SolvePart2(input); !reflect.DeepEqual(got, want) {
 		t.Errorf("SolvePart2() = %T(%v), want %T(%v)", got, got, want, want)
+	}
+}
+
+func Test_ParseNumbers(t *testing.T) {
+	want := []string{"7", "4", "9", "5", "11", "17", "23", "2", "0", "14", "21", "24", "10", "16", "13", "6", "15", "25", "12", "22", "18", "20", "8", "19", "3", "26", "1"}
+
+	input := shared.ReadInputLines("sample1")
+
+	if got := ParseNumbers(input[0]); !reflect.DeepEqual(got, want) {
+		t.Errorf("ParseNumbers() = %T(%v), want %T(%v)", got, got, want, want)
+	}
+}
+
+func Test_ParseBoards(t *testing.T) {
+	want := [][][]string{
+		{
+			{"22", "13", "17", "11", "0"},
+			{"8", "2", "23", "4", "24"},
+			{"21", "9", "14", "16", "7"},
+			{"6", "10", "3", "18", "5"},
+			{"1", "12", "20", "15", "19"},
+		},
+		{
+			{"3", "15", "0", "2", "22"},
+			{"9", "18", "13", "17", "5"},
+			{"19", "8", "7", "25", "23"},
+			{"20", "11", "10", "24", "4"},
+			{"14", "21", "16", "12", "6"},
+		},
+		{
+			{"14", "21", "17", "24", "4"},
+			{"10", "16", "15", "9", "19"},
+			{"18", "8", "23", "26", "20"},
+			{"22", "11", "13", "6", "5"},
+			{"2", "0", "12", "3", "7"},
+		},
+	}
+
+	input := shared.ReadInputLines("sample1")
+
+	if got := ParseBoards(input[2:]); !reflect.DeepEqual(got, want) {
+		t.Errorf("ParseBoards() = %T(%v), want %T(%v)", got, got, want, want)
+	}
+}
+
+func Test_CheckBoards(t *testing.T) {
+	want := [][5][5]bool{
+		{
+			{false, true, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+		},
+		{
+			{false, false, false, false, false},
+			{false, false, true, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+		},
+		{
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, true, false, false},
+			{false, false, false, false, false},
+		},
+	}
+
+	input := ParseBoards(shared.ReadInputLines("sample1")[2:])
+	got := make([][5][5]bool, len(input))
+
+	if CheckBoards("13", &input, &got); !reflect.DeepEqual(got, want) {
+		t.Errorf("CheckBoards() = %T(%v), want %T(%v)", got, got, want, want)
 	}
 }
 
