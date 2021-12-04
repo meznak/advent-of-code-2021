9f203af8e9ac837ae43a3743be7d92fa7d564a82
Nathan Plamondon <nplamondon@gmail.com>
2021-12-03 21:55:50 -0700

    day04: Start

---
+++ day04/main.go
@@ -0,0 +1,29 @@
+/*
+Advent of Code 2020 day 03
+Binary Diagnostic
+*/
+
+package main
+
+import (
+	"aoc2021/shared"
+	"fmt"
+)
+
+// Convert binary to "gamma" and "epsilon" numbers
+func SolvePart1(input []string) int {
+
+	return -1
+}
+
+//
+func SolvePart2(input []string) int {
+	return -1
+}
+
+func main() {
+	input := shared.ReadInputLines("day03/input")
+
+	fmt.Printf("Part 1: %v\n", SolvePart1(input))
+	fmt.Printf("Part 2: %v\n", SolvePart2(input))
+}
