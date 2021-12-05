a94bae511dc9cf5ba3771c7aaab2753644e4721c
Nathan Plamondon <nplamondon@gmail.com>
2021-12-05 00:16:18 -0700

    day05: Apply lines

--- day05/main.go
+++ day05/main.go
@@ -12,37 +12,20 @@ import (
 	"strings"
 )
 
+type Vector struct {
+	x int
+	y int
+}
+
 // Count line intersections
 func SolvePart1(input []string) int {
-	const grid_size int = 9
-	// Create grid
-	grid := make([][grid_size]int, grid_size)
+	points := make(map[Vector]int, 0)
 
 	// Parse lines
 	lines := ParseLines(input)
+
 	// Apply lines
-	for _, line := range lines {
-
-		switch {
-		case line[2] > line[0]: // right
-			for x := line[0]; x < line[2]; x++ {
-				grid[x][line[1]]++
-			}
-		case line[3] > line[1]: // down
-			for x := line[1]; x < line[3]; x++ {
-				grid[x][line[0]]++
-			}
-		case line[2] > line[0]: // left
-			for x := line[0]; x < line[2]; x++ {
-				grid[x][line[1]]++
-			}
-		case line[3] > line[1]: // up
-			for x := line[0]; x < line[2]; x++ {
-				grid[x][line[0]]++
-			}
-		}
-
-	}
+	ApplyLines(&points, &lines)
 
 	// Count intersections
 
@@ -58,6 +41,10 @@ func SolvePart2(input []string) int {
 func ParseLines(input []string) (out [][]int) {
 	// 0,9 -> 5,9
 	for _, line := range input {
+		if len(line) < 10 {
+			continue
+		}
+
 		move := make([]int, 0)
 		move_s := strings.Split(line, " -> ")
 		start_s := strings.Split(move_s[0], ",")
@@ -78,8 +65,46 @@ func ParseLines(input []string) (out [][]int) {
 	return
 }
 
+func ApplyLines(points *map[Vector]int, lines *[][]int) {
+	for _, line := range *lines {
+		// x1 y1 x2 y2
+		//  0  1  2  3
+
+		switch {
+		case line[2] > line[0]: // right
+			for x := line[0]; x <= line[2]; x++ {
+				point := Vector{x, line[1]}
+				AddPoint(points, point)
+			}
+		case line[3] > line[1]: // down
+			for y := line[1]; y <= line[3]; y++ {
+				point := Vector{line[0], y}
+				AddPoint(points, point)
+			}
+		case line[2] < line[0]: // left
+			for x := line[0]; x >= line[2]; x-- {
+				point := Vector{x, line[1]}
+				AddPoint(points, point)
+			}
+		case line[3] < line[1]: // up
+			for y := line[1]; y >= line[3]; y-- {
+				point := Vector{line[0], y}
+				AddPoint(points, point)
+			}
+		}
+	}
+}
+
+func AddPoint(points *map[Vector]int, point Vector) {
+	if _, exists := (*points)[point]; !exists {
+		(*points)[point] = 0
+	}
+
+	(*points)[point]++
+}
+
 func main() {
-	input := shared.ReadInputLines("day04/input")
+	input := shared.ReadInputLines("day05/input")
 	input = append(input, fmt.Sprintf("\n"))
 
 	fmt.Printf("Part 1: %v\n", SolvePart1(input))
