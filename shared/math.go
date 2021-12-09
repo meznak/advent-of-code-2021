fed78abba094c898c4d27f5dc116d66eecd136c4
Nathan Plamondon <nplamondon@gmail.com>
2021-12-09 11:25:45 -0700

    Merge math refactoring

--- shared/math.go
+++ shared/math.go
@@ -1,5 +1,13 @@
 package shared
 
+func AbsInt(number int) int {
+	if number < 0 {
+		return -1 * number
+	}
+
+	return number
+}
+
 func MinInt(nums *[]int) (int, int) {
 	min := (*nums)[0]
 	pos := 0
@@ -26,4 +34,22 @@ func MaxInt(nums *[]int) (int, int) {
 	}
 
 	return pos, max
+}
+
+func SumInt(nums *[]int) (sum int) {
+	for _, v := range *nums {
+		sum += v
+	}
+
+	return
+}
+
+func SumVerticalInt(nums *[][]int, col int, ch chan int) {
+	sum := 0
+
+	for i := range *nums {
+		sum += (*nums)[i][col]
+	}
+
+	ch <- sum
 }
